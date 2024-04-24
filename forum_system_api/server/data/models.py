a391825dfeb63e39c676b904b8bcda44fac419b8
eahristovaa5 <eahristovaa@gmail.com>
2024-04-24 16:31:24 +0300

    123

---
+++ forum_system_api/server/data/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel): # - Vladi
+    pass 
+
+class Category(BaseModel): # - Valkata
+    pass 
+
+class Message(BaseModel): # - Valkata
+    pass 
+
+class Topic(BaseModel): # - Elena
+    pass 
+
+class Reply(BaseModel): # - Elena
+    pass 
