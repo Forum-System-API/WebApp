792e9368e33aaf78d9e68284db5e570d525429f8
eahristovaa5 <eahristovaa@gmail.com>
2024-04-25 09:34:03 +0300

    service_topic

--- forum_system_api/server/services/topic_service.py
+++ forum_system_api/server/services/topic_service.py
@@ -1,1 +1,47 @@
-# TO FOLLOW - Elena
+# Elena
+from data.models import Topic, UpdateTopic, Reply
+from data.database import insert_query, read_query, update_query
+
+# TO DO: implement logic which will show a list of all of the topics 
+def all():
+    pass
+
+# TO DO: implement logic which will show a specific topic 
+def get_by_id():
+    pass
+
+# TO DO: implement logic which will show a list of the topics in a specific category
+def get_by_category():
+    pass
+
+# TO DO: implement logic which will sort the current showing topics alphabetically
+def sort():
+    pass
+
+# TO DO: implement logic which will show a topic with all of its replies
+def get_with_replies():
+    pass
+
+# TO DO: implement logic which will create a new topic
+def create():
+    pass
+
+# TO DO: implement logic which will check if a specific topic exist already
+def exist():
+    pass
+
+# TO DO: implement logic which will update the title of the topic or its category
+def update():
+    pass
+
+# TO DO: implement logic which will remove a reply from a topic
+def remove_reply():
+    pass
+
+# TO DO: implement logic which will delete a topic
+def delete():
+    pass
+
+# TO DO: implement logic which will stop the replies for a specific topic
+def lock_topic():
+    pass
