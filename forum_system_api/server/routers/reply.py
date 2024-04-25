6f1ebe890051d417aaae92eb0ca579937f21b5e7
eahristovaa5 <eahristovaa@gmail.com>
2024-04-25 09:32:12 +0300

    router_reply

--- forum_system_api/server/routers/reply.py
+++ forum_system_api/server/routers/reply.py
@@ -1,1 +1,21 @@
-# TO FOLLOW - Elena
+# Elena
+from fastapi import APIRouter, Response
+from data.models import Reply, UpdateReply
+
+
+replies_router = APIRouter(prefix='/topics')
+
+# TO DO: implement endpoint which will create a new reply
+@replies_router.post('/', status_code=201)
+def cerate_reply():
+       pass
+
+# TO DO: implement endpoint which will upvote/downvote a reply
+@replies_router.put()
+def get_topic_by_id():
+        pass
+
+# TO DO: implement endpoint which will mark a reply as the best one
+@replies_router.put()
+def create_topic():
+    pass
