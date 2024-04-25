b3a0cb8770aba156a506b9cf6c727c243e8f8976
eahristovaa5 <eahristovaa@gmail.com>
2024-04-25 10:48:55 +0300

    change

--- forum_system_api/server/routers/reply.py
+++ forum_system_api/server/routers/reply.py
@@ -6,16 +6,16 @@ from data.models import Reply, UpdateReply
 replies_router = APIRouter(prefix='/replies')
 
 # TO DO: implement endpoint which will create a new reply
-@replies_router.post('/', status_code=201)
-def cerate_reply():
-       pass
+# @replies_router.post('/', status_code=201)
+# def cerate_reply():
+#        pass
 
 # TO DO: implement endpoint which will upvote/downvote a reply
-@replies_router.put()
-def get_topic_by_id():
-        pass
+# @replies_router.put()
+# def get_topic_by_id():
+#         pass
 
 # TO DO: implement endpoint which will mark a reply as the best one
-@replies_router.put()
-def create_topic():
-    pass
+# @replies_router.put()
+# def create_topic():
+#     pass
