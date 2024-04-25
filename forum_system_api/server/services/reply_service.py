dad7f1bf0b6cf49fbcc9325ac6a87a8d10bb32c4
eahristovaa5 <eahristovaa@gmail.com>
2024-04-25 09:33:32 +0300

    service_reply

--- forum_system_api/server/services/reply_service.py
+++ forum_system_api/server/services/reply_service.py
@@ -1,1 +1,40 @@
-# TO FOLLOW - Elena
+# Elena
+from data.database import insert_query, read_query, update_query
+from data.models import Reply, UpdateReply, Topic
+
+
+# TO DO: implement logic which will show all the replies of a specific topic
+def get_by_topic():
+    pass
+
+# TO DO: implement logic which will show a specific reply
+def get_by_id():
+    pass
+
+# TO DO: implement logic which will sort the replies by time of posting
+def sort():
+    pass
+
+# TO DO: implement logic which will create a new reply
+def create():
+    pass
+
+# TO DO: implement logic which will check if a reply has been posted already
+def exist():
+    pass
+
+# TO DO: implement logic which will update the text of a reply
+def update():
+    pass
+
+# TO DO: implement logic which will delete a reply
+def delete():
+    pass
+
+# TO DO: implement logic which will upvote or downvote a reply
+def upvote_downvote():
+    pass
+
+# TO DO: implement logic which will mark a reply as the best one
+def best():
+    pass
