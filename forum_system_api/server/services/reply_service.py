c0c24d169a2f8bf2420bae1002883fbdfa9f7af4
eahristovaa5 <eahristovaa@gmail.com>
2024-05-02 09:58:32 +0300

    reply

--- forum_system_api/server/services/reply_service.py
+++ forum_system_api/server/services/reply_service.py
@@ -1,40 +1,56 @@
-# # Elena - changes to follow
-# from data.database import insert_query, read_query, update_query
-# from data.models import Reply, UpdateReply, Topic
+from data.database import insert_query, read_query, update_query
+from data.models import Reply
 
 
-# # TO DO: implement logic which will show all the replies of a specific topic
-# def get_by_topic():
+def get_by_id(id: int):
+    data = read_query(
+        '''SELECT reply_id, text, upvotes, downvotes, topic_id, user_id, timestamp
+            FROM replies 
+            WHERE reply_id = ?''', (id,))
+
+    return next((Reply.from_query_result(*row) for row in data), None)
+
+def topic_exists(topic_id: int) -> bool:
+    return any(
+        read_query(
+            'SELECT topic_id, title from topics where topic_id = ?',
+            (topic_id,)))
+
+def create(reply: Reply):
+    generated_id = insert_query(
+        'INSERT INTO replies(text,upvotes,downvotes,topic_id,user_id,timestamp) VALUES(?,?,?,?,?,?)',
+        (reply.text, reply.upvotes, reply.downvotes, reply.topic_id, reply.user_id, reply.timestamp))
+
+    reply.id = generated_id
+
+    # add_replies_to_topic(reply.id, reply.topic_id)
+
+    return reply
+
+# def add_replies_to_topic():
 #     pass
 
-# # TO DO: implement logic which will show a specific reply
-# def get_by_id():
-#     pass
+def update(old: Reply, new: Reply):
+    merged = Reply(
+        id=old.id,
+        text=new.text or old.text,
+        upvotes=old.upvotes,
+        downvotes=old.downvotes,
+        topic_id=old.topic_id,
+        user_id=old.user_id,
+        timestamp=old.timestamp)
 
-# # TO DO: implement logic which will sort the replies by time of posting
-# def sort():
-#     pass
+    update_query(
+        '''UPDATE replies SET
+           text = ?
+           WHERE id = ? 
+        ''',
+        (merged.id, merged.text, merged.upvotes, merged.downvotes, merged.topic_id, merged.user_id, merged.timestamp))
 
-# # TO DO: implement logic which will create a new reply
-# def create():
-#     pass
+    return merged
 
-# # TO DO: implement logic which will check if a reply has been posted already
-# def exist():
-#     pass
 
-# # TO DO: implement logic which will update the text of a reply
-# def update():
-#     pass
+def delete(reply_id):
+    update_query('DELETE FROM replies WHERE id = ?', (reply_id,))
 
-# # TO DO: implement logic which will delete a reply
-# def delete():
-#     pass
 
-# # TO DO: implement logic which will upvote or downvote a reply
-# def upvote_downvote():
-#     pass
-
-# # TO DO: implement logic which will mark a reply as the best one
-# def best():
-#     pass
