a4e567e2c053781490f2086aaf58626f74aeb411
eahristovaa5 <eahristovaa@gmail.com>
2024-05-02 09:57:29 +0300

    reply

--- forum_system_api/server/routers/reply.py
+++ forum_system_api/server/routers/reply.py
@@ -1,21 +1,37 @@
-# # Elena - changes to follow
-# from fastapi import APIRouter, Response
-# from data.models import Reply, UpdateReply
+from fastapi import APIRouter, Response
+from data.models import Reply
+from services import reply_service
 
 
-# replies_router = APIRouter(prefix='/replies')
+reply_router = APIRouter(prefix='/replies')
 
-# # TO DO: implement endpoint which will create a new reply
-# # @replies_router.post('/', status_code=201)
-# # def cerate_reply():
-# #        pass
+# creates a new reply to a certain topic
+@reply_router.post('/', status_code=201)
+def create_reply(reply: Reply):
+     if not reply_service.topic_exists(reply.topic_id):
+        return Response(status_code=400,
+                        content=f'Topic {reply.topic_id} does not exist')
 
-# # TO DO: implement endpoint which will upvote/downvote a reply
-# # @replies_router.put()
-# # def get_topic_by_id():
-# #         pass
+     return reply_service.create(reply)
 
-# # TO DO: implement endpoint which will mark a reply as the best one
-# # @replies_router.put()
-# # def create_topic():
-# #     pass
+
+# updates a repply
+@reply_router.put('/{id}')
+def update_reply(id: int, reply: Reply):
+     if not reply_service.category_exists(reply.topic_id):
+        return Response(status_code=400,
+                        content=f'Topic {reply.topic_id} does not exist')
+
+     existing_reply = reply_service.get_by_id(id)
+     if existing_reply is None:
+        return Response(status_code=404)
+     else:
+        return reply_service.update(existing_reply, reply)
+
+
+# deletes a repply
+@reply_router.delete('/{id}')
+def delete_reply(id: int):
+    reply_service.delete(id)
+
+    return Response(status_code=204)
