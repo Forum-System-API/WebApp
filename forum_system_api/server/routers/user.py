7e9d7b4a4c17e4dc2732421282df1a58b41e4609
Valentin <kakvoo1@gmail.com>
2024-05-03 12:54:58 +0100

    -

--- forum_system_api/server/routers/user.py
+++ forum_system_api/server/routers/user.py
@@ -9,29 +9,28 @@ users_router = APIRouter(prefix='/users')
 
 
 @users_router.get('/')
-def get_users(sort: str | None=None):
-    
+def get_users(sort: str | None = None):
     users = user_service.all()
-    
+
     if sort and (sort == 'asc' or sort == 'desc'):
-        return users_services.sort(users, reverse=sort=='desc')
+        return user_service.sort(users, reverse=sort == 'desc')
     else:
         return users
+
 
 @users_router.post('/register')
 def register(data: LoginData, response: Response):
     user = user_service.create(data.username, data.password)
-    
-   
+
     if user:
         return "Registration completed!"
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
-        return{"message":f"Username {data.username} is taken." }
-    
-    
+        return {"message": f"Username {data.username} is taken."}
+
+
 @users_router.post('/login')
-def login(data: LoginData,):
+def login(data: LoginData, ):
     user = user_service.try_login(data.username, data.password)
 
     if user:
@@ -43,12 +42,12 @@ def login(data: LoginData,):
             user.role = Role.ORDINARY_USER
             token = user_service.create_token(user)
             return {'token': token}
-    
+
     else:
         raise HTTPException(status_code=401, detail='Invalid username or password')
-    
+
 
 @users_router.get('/info')
 def user_info(x_token: str = Header()):
     user = get_user_or_raise_401(x_token)
-    return {"username":user.username, "role":user.role}
+    return {"username": user.username, "role": user.role}
