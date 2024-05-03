d586f1a070b1f2ec7389593b002c766cc7f6e62a
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-03 14:32:10 +0300

    12

--- forum_system_api/server/routers/user.py
+++ forum_system_api/server/routers/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Response, status, HTTPException, Header
 from data.models import User, LoginData, Role
-from services import users_services
+from services import user_service
 import jwt
 from services.config import secret_key
 from common.auth import get_user_or_raise_401
@@ -11,7 +11,7 @@ users_router = APIRouter(prefix='/users')
 @users_router.get('/')
 def get_users(sort: str | None=None):
     
-    users = users_services.all()
+    users = user_service.all()
     
     if sort and (sort == 'asc' or sort == 'desc'):
         return users_services.sort(users, reverse=sort=='desc')
@@ -20,7 +20,7 @@ def get_users(sort: str | None=None):
 
 @users_router.post('/register')
 def register(data: LoginData, response: Response):
-    user = users_services.create(data.username, data.password)
+    user = user_service.create(data.username, data.password)
     
    
     if user:
@@ -32,16 +32,16 @@ def register(data: LoginData, response: Response):
     
 @users_router.post('/login')
 def login(data: LoginData,):
-    user = users_services.try_login(data.username, data.password)
+    user = user_service.try_login(data.username, data.password)
 
     if user:
         if user.username == "admin":
             user.role = Role.ADMIN
-            token = users_services.create_token(user)
+            token = user_service.create_token(user)
             return {'token': token}
         else:
             user.role = Role.ORDINARY_USER
-            token = users_services.create_token(user)
+            token = user_service.create_token(user)
             return {'token': token}
     
     else:
