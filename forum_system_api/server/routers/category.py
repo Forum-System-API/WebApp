36b85a6030b018d0413325b9769b74b3c990a8aa
eahristovaa5 <eahristovaa@gmail.com>
2024-05-06 17:15:45 +0300

    category_name

--- forum_system_api/server/routers/category.py
+++ forum_system_api/server/routers/category.py
@@ -12,13 +12,13 @@ def show_categories():
     return categories
 
 
-@category_router.get('/name/{category_name}')
-def show_category_by_name(category_name: str):
-    if not category_service.category_name_exists(category_name):
+@category_router.get('/name/{name}')
+def show_category_by_name(name: str):
+    if not category_service.category_name_exists(name):
         return Response(status_code=400,
-                        content=f'Category with name "{category_name}" does not exist')
+                        content=f'Category with name "{name}" does not exist')
 
-    category = category_service.find_by_name(category_name)
+    category = category_service.find_by_name(name)
     return category
 
 
@@ -44,12 +44,12 @@ def delete_category_by_id(category_id: int, x_token: str = Header()):
     pass
 
 
-@category_router.delete('/{category_name}')
-def delete_category_by_name(category_name: str, x_token: str = Header()):
+@category_router.delete('/{name}')
+def delete_category_by_name(name: str, x_token: str = Header()):
     #user = get_user_or_raise_401(x_token)
-    if not category_service.category_name_exists(category_name):
+    if not category_service.category_name_exists(name):
         return Response(status_code=400,
-                        content=f'Category with id #{category_name} does not exist')
+                        content=f'Category with id #{name} does not exist')
 
     #if not user.role != "admin":
         #pass
