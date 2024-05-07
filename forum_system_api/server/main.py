cc152a460af7ad25504581b9a15c3259eaff2737
eahristovaa5 <eahristovaa@gmail.com>
2024-05-07 12:37:29 +0300

    main

--- forum_system_api/server/main.py
+++ forum_system_api/server/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-# Vladi
 from routers.user import users_router
-# # Elena - changes to follow
 from routers.topic import topics_router
-# from routers.reply import replies_router
-# Valkata
+from routers.reply import replies_router
 from routers.category import category_router
 from routers.message import message_router
 import uvicorn
@@ -12,12 +9,10 @@ import uvicorn
 
 app = FastAPI()
 
-# Vladi
+
 app.include_router(users_router)
-# Elena - changes to follow
 app.include_router(topics_router)
-# app.include_router(replies_router)
-# Valkata
+app.include_router(replies_router)
 app.include_router(category_router)
 app.include_router(message_router)
 
