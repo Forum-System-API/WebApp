ffa50a40d0c09b0ab6ab24be40348660f27719bb
eahristovaa5 <eahristovaa@gmail.com>
2024-05-07 12:35:29 +0300

    reply

--- forum_system_api/server/routers/reply.py
+++ forum_system_api/server/routers/reply.py
@@ -1,37 +1,32 @@
 from fastapi import APIRouter, Response
 from data.models import Reply
-from services import reply_service
-
+from services import reply_service, topic_service
 
 replies_router = APIRouter(prefix='/replies')
 
-# # creates a new reply to a certain topic
-# @replies_router.post('/', status_code=201)
-# def create_reply(reply: Reply):
-#      if not reply_service.topic_exists(reply.topic_id):
-#         return Response(status_code=400,
-#                         content=f'Topic {reply.topic_id} does not exist')
+# available for: admin, user
+@replies_router.post('/', status_code=201)
+def create_reply(reply: Reply):
+   if not topic_service.exists(reply.topic_id):
+      return Response(status_code=400, content=f'Topic {reply.topic_id} does not exist.')
 
-#      return reply_service.create(reply)
+   return reply_service.create(reply)
 
+# available for: user
+@replies_router.put('/{reply_id}')
+def update_reply(reply_id: int, reply: Reply):
+   if not topic_service.exists(reply.topic_id):
+      return Response(status_code=400, content=f'Topic {reply.topic_id} does not exist.')
 
-# # updates a repply
-# @replies_router.put('/{id}')
-# def update_reply(id: int, reply: Reply):
-#      if not reply_service.category_exists(reply.topic_id):
-#         return Response(status_code=400,
-#                         content=f'Topic {reply.topic_id} does not exist')
+   existing_reply = reply_service.get_by_id(reply_id)
+   if existing_reply is None:
+      return Response(status_code=404)
+   else:
+      return reply_service.update(existing_reply, reply)
 
-#      existing_reply = reply_service.get_by_id(id)
-#      if existing_reply is None:
-#         return Response(status_code=404)
-#      else:
-#         return reply_service.update(existing_reply, reply)
+# available for: user
+@replies_router.delete('/{reply_id}')
+def delete_reply(reply_id: int):
+   reply_service.delete(reply_id)
 
-
-# # deletes a repply - who deletes
-# @replies_router.delete('/{id}')
-# def delete_reply(id: int):
-#     reply_service.delete(id)
-
-#     return Response(status_code=204)
+   return Response(status_code=204)
