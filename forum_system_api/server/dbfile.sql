6ad97cde8dba9a53fe6cef685fb939f851654aa3
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-07 19:16:58 +0300

    UPDATED DATABASE

--- forum_system_api/server/dbfile.sql
+++ forum_system_api/server/dbfile.sql
@@ -180,10 +180,13 @@ INSERT INTO `webapp`.`categories` (`name`,`is_private`,`is_locked`) VALUES ('Nat
 INSERT INTO `webapp`.`categories` (`name`,`is_private`,`is_locked`) VALUES ('Cooking',0,0);
 
 INSERT INTO `webapp`.`users` ( `username`,`password`,`role`) VALUES 
-('admin','9cd34e3d1cdef8d5bab590f05b00dcbc9c7e20d7625b069d72aed16566a59ca7','admin'),
-('Joey_Ramone','dda69783f28fdf6f1c5a83e8400f2472e9300887d1dffffe12a07b92a3d0aa25','basic_user'),
-('MoSalah','8eff929c533e9a6ef1261ed11572b17dfac3947722808a3d3803a781cafdc186','basic_user'),
-('Sherlock','f90c0f69179ced9c447b111dd5235f5279b28674463a0a607ef8dda5909a8747','basic_user');
+('admin', '9cd34e3d1cdef8d5bab590f05b00dcbc9c7e20d7625b069d72aed16566a59ca7', 'admin'),
+    ('Joey_Ramone', 'dda69783f28fdf6f1c5a83e8400f2472e9300887d1dffffe12a07b92a3d0aa25', 'basic_user'),
+    ('MoSalah', '8eff929c533e9a6ef1261ed11572b17dfac3947722808a3d3803a781cafdc186', 'basic_user'),
+    ('Sherlock', 'f90c0f69179ced9c447b111dd5235f5279b28674463a0a607ef8dda5909a8747', 'basic_user'),
+    ('Eric', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'basic_user'),
+    ('slave', '61e81a50ea9de3c9a48c95d6bb4aecc196de64d696fdd24f647afe18f9909d7c', 'basic_user'),
+    ('skankhunt42', 'c4a1e6f3639a098e68060c2a31cfc3567bf68b3ec1bb4f843a34f7499caf2998', 'basic_user');
 
 INSERT INTO `webapp`.`topics` (`title`,`category_id`,`user_id`) VALUES ('F1 Miami GP FP1',2,1);
 INSERT INTO `webapp`.`topics` (`title`,`category_id`,`user_id`) VALUES ('F1 Miami GP Sprint Quali',2,1);
