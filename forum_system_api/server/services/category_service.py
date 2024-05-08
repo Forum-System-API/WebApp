768ace0e6fdcdca59a178d215afee0c5455618a1
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-08 18:47:15 +0300

    change category service

--- forum_system_api/server/services/category_service.py
+++ forum_system_api/server/services/category_service.py
@@ -4,16 +4,16 @@ from mariadb import IntegrityError
 
 
 def all():
-    data = read_query('SELECT category_id, name FROM categories')
+    data = read_query('SELECT category_id, category_name FROM categories')
     formatted_data = [
-        {"category_id": row[0], "name": row[1]} for row in data
+        {"category_id": row[0], "category_name": row[1]} for row in data
     ]
     return formatted_data
 
 
 def find_by_name(category_name: str) -> Category | None:
     data = read_query(
-        'SELECT category_id, name FROM categories WHERE name = ?',
+        'SELECT category_id, category_name FROM categories WHERE name = ?',
         (category_name,))
 
     return next((Category.from_query_result(*row) for row in data), None)
@@ -21,7 +21,7 @@ def find_by_name(category_name: str) -> Category | None:
 
 def find_by_id(category_id: int) -> tuple[Category | None, list[Topic] | None]:
     category_data = read_query(
-        'SELECT category_id, name FROM categories WHERE category_id = ?',
+        'SELECT category_id, category_name FROM categories WHERE category_id = ?',
         (category_id,))
     topic_data = read_query(
         'SELECT topic_id, title, category_id, user_id, date_time FROM topics WHERE category_id = ?',
@@ -37,14 +37,14 @@ def find_by_id(category_id: int) -> tuple[Category | None, list[Topic] | None]:
 def category_name_exists(category_name: str) -> bool:
     return any(
         read_query(
-            'SELECT category_id, name FROM categories WHERE name = ?',
+            'SELECT category_id, category_name FROM categories WHERE name = ?',
             (category_name,)))
 
 
 def category_id_exists(category_id: int) -> bool:
     return any(
         read_query(
-            'SELECT category_id, name FROM categories WHERE category_id = ?',
+            'SELECT category_id, category_name FROM categories WHERE category_id = ?',
             (category_id,)))
 
 
@@ -54,7 +54,7 @@ def delete_category(name):  #Untested
 
 def create(name: str):
     existing_category = read_query(
-        'SELECT name FROM categories WHERE name = ?', (name,)
+        'SELECT category_name FROM categories WHERE name = ?', (name,)
     )
 
     if existing_category:
@@ -62,7 +62,7 @@ def create(name: str):
 
     try:
         generated_id = insert_query(
-            'INSERT INTO categories(name) VALUES(?)', (name,))
+            'INSERT INTO categories(category_name) VALUES(?)', (name,))
 
         return Category(category_id=generated_id, category_name=name)
     except IntegrityError:
