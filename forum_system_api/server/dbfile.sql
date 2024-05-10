92a1377b1b2a79ea77bd66f822c233510f143cc0
Vladislav <vladislav.rashkoff@gmail.com>
2024-05-10 20:45:27 +0300

    latest schema

--- forum_system_api/server/dbfile.sql
+++ forum_system_api/server/dbfile.sql
@@ -11,103 +11,48 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema webapp
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `webapp` DEFAULT CHARACTER SET latin1 ;
+CREATE SCHEMA IF NOT EXISTS `webapp` ;
 USE `webapp` ;
-
--- -----------------------------------------------------
--- Table `webapp`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webapp`.`categories` (
-  `category_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `category_name` VARCHAR(45) NOT NULL,
-  `is_private` TINYINT(4) NOT NULL DEFAULT 0,
-  `is_locked` TINYINT(4) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`category_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 7
-DEFAULT CHARACTER SET = latin1;
-
 
 -- -----------------------------------------------------
 -- Table `webapp`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `webapp`.`users` (
-  `user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` TEXT NOT NULL,
   `role` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 8
-DEFAULT CHARACTER SET = latin1;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `webapp`.`categories_access`
+-- Table `webapp`.`categories`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webapp`.`categories_access` (
-  `category_id` INT(11) NOT NULL,
-  `user_id` INT(11) NOT NULL,
-  `can_read` TINYINT(4) NOT NULL DEFAULT 1,
-  `can_write` TINYINT(4) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`category_id`, `user_id`),
-  INDEX `fk_categories_has_users_users1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_categories_has_users_categories_idx` (`category_id` ASC) VISIBLE,
-  CONSTRAINT `fk_categories_has_users_categories`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `webapp`.`categories` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_categories_has_users_users1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `webapp`.`users` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `webapp`.`messages`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `webapp`.`messages` (
-  `message_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `text` TEXT NOT NULL,
-  `timestamp` DATETIME NOT NULL,
-  `sender_id` INT(11) NOT NULL,
-  `recipient_id` INT(11) NOT NULL,
-  PRIMARY KEY (`message_id`),
-  INDEX `fk_messages_users1_idx` (`sender_id` ASC) VISIBLE,
-  INDEX `fk_messages_users2_idx` (`recipient_id` ASC) VISIBLE,
-  CONSTRAINT `fk_messages_users1`
-    FOREIGN KEY (`sender_id`)
-    REFERENCES `webapp`.`users` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_users2`
-    FOREIGN KEY (`recipient_id`)
-    REFERENCES `webapp`.`users` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+CREATE TABLE IF NOT EXISTS `webapp`.`categories` (
+  `category_id` INT NOT NULL AUTO_INCREMENT,
+  `category_name` VARCHAR(45) NOT NULL,
+  `is_private` TINYINT NOT NULL DEFAULT 0,
+  `is_locked` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`category_id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `webapp`.`topics`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `webapp`.`topics` (
-  `topic_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `topic_id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(200) NOT NULL,
   `date_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `category_id` INT(11) NOT NULL,
-  `user_id` INT(11) NOT NULL,
+  `category_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   `best_reply` TEXT NULL DEFAULT 'no best reply yet.',
-  `is_locked` TINYINT(4) NOT NULL DEFAULT 0,
-  `is_private` TINYINT(4) NOT NULL DEFAULT 0,
+  `is_locked` TINYINT NOT NULL DEFAULT 0,
+  `is_private` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`topic_id`),
-  INDEX `fk_topics_categories1_idx` (`category_id` ASC) VISIBLE,
-  INDEX `fk_topics_users1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_topics_categories1_idx` (`category_id` ASC) ,
+  INDEX `fk_topics_users1_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_topics_categories1`
     FOREIGN KEY (`category_id`)
     REFERENCES `webapp`.`categories` (`category_id`)
@@ -118,23 +63,21 @@ CREATE TABLE IF NOT EXISTS `webapp`.`topics` (
     REFERENCES `webapp`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = latin1;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `webapp`.`replies`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `webapp`.`replies` (
-  `reply_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `reply_id` INT NOT NULL AUTO_INCREMENT,
   `text` TEXT NOT NULL,
   `date_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `topic_id` INT(11) NOT NULL,
-  `user_id` INT(11) NOT NULL,
+  `topic_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`reply_id`),
-  INDEX `fk_replies_topics1_idx` (`topic_id` ASC) VISIBLE,
-  INDEX `fk_replies_users1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_replies_topics1_idx` (`topic_id` ASC) ,
+  INDEX `fk_replies_users1_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_replies_topics1`
     FOREIGN KEY (`topic_id`)
     REFERENCES `webapp`.`topics` (`topic_id`)
@@ -145,40 +88,85 @@ CREATE TABLE IF NOT EXISTS `webapp`.`replies` (
     REFERENCES `webapp`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = latin1;
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `webapp`.`messages`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webapp`.`messages` (
+  `message_id` INT NOT NULL AUTO_INCREMENT,
+  `text` TEXT NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  `sender_id` INT NOT NULL,
+  `recipient_id` INT NOT NULL,
+  PRIMARY KEY (`message_id`),
+  INDEX `fk_messages_users1_idx` (`sender_id` ASC) ,
+  INDEX `fk_messages_users2_idx` (`recipient_id` ASC) ,
+  CONSTRAINT `fk_messages_users1`
+    FOREIGN KEY (`sender_id`)
+    REFERENCES `webapp`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_messages_users2`
+    FOREIGN KEY (`recipient_id`)
+    REFERENCES `webapp`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `webapp`.`categories_has_users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webapp`.`categories_has_users` (
+  `category_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `access_type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`category_id`, `user_id`),
+  INDEX `fk_categories_has_users_users1_idx` (`user_id` ASC) ,
+  INDEX `fk_categories_has_users_categories_idx` (`category_id` ASC) ,
+  CONSTRAINT `fk_categories_has_users_categories`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `webapp`.`categories` (`category_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_categories_has_users_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `webapp`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `webapp`.`votes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `webapp`.`votes` (
-  `user_id` INT(11) NOT NULL,
-  `reply_id` INT(11) NOT NULL,
+  `user_id` INT NOT NULL,
+  `reply_id` INT NOT NULL,
   `type_of_vote` VARCHAR(45) NULL DEFAULT 'no votes yet.',
   PRIMARY KEY (`user_id`, `reply_id`),
-  INDEX `fk_users_has_replies_replies1_idx` (`reply_id` ASC) VISIBLE,
-  INDEX `fk_users_has_replies_users1_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_users_has_replies_replies1`
-    FOREIGN KEY (`reply_id`)
-    REFERENCES `webapp`.`replies` (`reply_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_users_has_replies_replies1_idx` (`reply_id` ASC) ,
+  INDEX `fk_users_has_replies_users1_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_users_has_replies_users1`
     FOREIGN KEY (`user_id`)
     REFERENCES `webapp`.`users` (`user_id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_replies_replies1`
+    FOREIGN KEY (`reply_id`)
+    REFERENCES `webapp`.`replies` (`reply_id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-
+-- Insert sample data
 
 INSERT INTO `webapp`.`categories` (`category_name`,`is_private`,`is_locked`) VALUES ('Arts',0,0);
 INSERT INTO `webapp`.`categories` (`category_name`,`is_private`,`is_locked`) VALUES ('Sports',0,0);
@@ -210,4 +198,3 @@ INSERT INTO `webapp`.`replies` (`text`,`date_time`,`topic_id`,`user_id`) VALUES 
 INSERT INTO `webapp`.`replies` (`text`,`date_time`,`topic_id`,`user_id`) VALUES ('Max Vestappen wins the sprint race.','2024-05-04 11:00:00',3,4);
 INSERT INTO `webapp`.`replies` (`text`,`date_time`,`topic_id`,`user_id`) VALUES ('Daniel Ricciardo P4 and points for the RB pilot.','2024-05-04 11:10:00',3,5);
 INSERT INTO `webapp`.`replies` (`text`,`date_time`,`topic_id`,`user_id`) VALUES ('Lando Norris first victory in F1 with the McLaren team.','2024-05-05 17:00:00',5,4);
-
