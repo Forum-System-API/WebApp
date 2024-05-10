ea345864dfdc266c65cd173aec1db5b97a683d84
eahristovaa5 <eahristovaa@gmail.com>
2024-05-10 18:42:07 +0300

    votes

--- forum_system_api/server/data/database.py
+++ forum_system_api/server/data/database.py
@@ -5,7 +5,7 @@ from mariadb.connections import Connection
 def _get_connection() -> Connection:
     return connect(
         user='root',
-        password='524_bazingams3',
+        password='',
         host='localhost',
         port=3306,
         database='webapp'
