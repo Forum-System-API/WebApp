fcc79092f4a14e3a087649d5efba5fe9248fd8ee
eahristovaa5 <eahristovaa@gmail.com>
2024-05-12 15:29:49 +0300

    category elena change

--- forum_system_api/server/routers/category.py
+++ forum_system_api/server/routers/category.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Response, status, HTTPException, Header
+from fastapi import APIRouter, Response, status, HTTPException, Header, Query
 from data.models import Category, Role, User, Categories_Access
-from services import category_service
+from services import category_service, topic_service
 from common.auth import get_user_or_raise_401
 
 category_router = APIRouter(prefix='/categories')
@@ -179,3 +179,26 @@ def privileged_users(category_id:int, x_token:str = Header()):
         raise HTTPException(status_code=403, detail="Admin credentials are required for this option!")
     
     return category_service.get_privileged(category_id)
+
+
+@category_router.get('/{category_id}/topics')
+def get_caegory_by_id(category_id: int,
+                   sort: str | None = None,
+                   sort_by: str | None = None,
+                   search: str | None = None,
+                   page: int = Query(1, gt=0),
+                   topics_per_page: int = Query(100, gt=0)):
+
+    topic_list = topic_service.get_topics_by_category(category_id)
+
+    if search:
+        topic_list = [topic for topic in topic_list if search.lower() in topic.title.lower()]
+
+    start = (page - 1) * topics_per_page
+    end = start + topics_per_page
+    topic_list = topic_list[start:end]
+
+    if sort and (sort == 'asc' or sort == 'desc') and sort_by:
+        topic_list = topic_service.sort(topic_list, reverse=sort == 'desc', attribute=sort_by)
+
+    return topic_list
